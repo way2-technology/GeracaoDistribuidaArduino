fcea7e44f5bd75f07aaae79e4cfc64a8d75bfafe
Thiago Cerutti <thiagocerutti@gmail.com>
2019-01-16 14:05:34 -0200

    testes de campo

--- GeracaoDistribuidaArduino.ino
+++ GeracaoDistribuidaArduino.ino
@@ -12,14 +12,15 @@ void setup() {
 
 float potenciaAnterior = 0;
 int razaoCiclica = 100;
+int razaoCiclicaAnterior = 100;
 
 void loop() {
   analogWrite(PINO, razaoCiclica);
-  delay(5000);
-  float tensao = analogRead(TENSAO);
-  float corrente = analogRead(CORRENTE);
+  delay(2000);
+  float tensao = (analogRead(TENSAO)*0.0048828125) * 1000 / 110;
+  float corrente = (analogRead(CORRENTE) - 512) * (0.05859375);
   float potenciaAtual = tensao * corrente;
-  
+
   Serial.print(tensao);
   Serial.print(" - ");
   Serial.print(corrente);
@@ -28,16 +29,26 @@ void loop() {
   Serial.print(" - ");
   Serial.print(razaoCiclica);
   Serial.println();
-  
-  if (potenciaAtual >= potenciaAnterior){
+
+  if ((potenciaAtual >= potenciaAnterior) && (razaoCiclica >= razaoCiclicaAnterior)) {
+    razaoCiclicaAnterior = razaoCiclica;
+    razaoCiclica += 10;
+  } else if ((potenciaAtual < potenciaAnterior) && (razaoCiclica >= razaoCiclicaAnterior)) {
+    razaoCiclicaAnterior = razaoCiclica;
     razaoCiclica -= 10;
-  } else {
+  } else if ((potenciaAtual >= potenciaAnterior) && (razaoCiclica <= razaoCiclicaAnterior)) {
+    razaoCiclicaAnterior = razaoCiclica;
+    razaoCiclica -= 10;
+  } else if ((potenciaAtual < potenciaAnterior) && (razaoCiclica <= razaoCiclicaAnterior)) {
+    razaoCiclicaAnterior = razaoCiclica;
     razaoCiclica += 10;
   }
+  
+  
   potenciaAnterior = potenciaAtual;
-  if (razaoCiclica > 255){
+  if (razaoCiclica > 255) {
     razaoCiclica = 255;
-  }else if (razaoCiclica < 0){
+  } else if (razaoCiclica < 0) {
     razaoCiclica = 0;
   }
 }
