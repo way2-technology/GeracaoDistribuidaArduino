3e5ede77b9f2a3dd1fa68098690b6e549786f883
Theo Menkes Reis <theomreis@gmail.com>
2019-01-17 00:36:17 +0000

    loop simplificado

--- GeracaoDistribuidaArduino.ino
+++ GeracaoDistribuidaArduino.ino
@@ -1,54 +1,44 @@
+template<class T> inline Print &operator <<(Print &obj, T arg) {
+  obj.print(arg);
+  return obj;
+} 
+
 #define PINO 9
 #define TENSAO A0
 #define CORRENTE A1
 
 void setup() {
-  TCCR1B = TCCR1B & 0b11111000 | 0X01;
+  TCCR1B = TCCR1B & 0b11111000 | 0X01; // Seta pinos 9 e 10 para 31250 Hz
+
   pinMode(PINO, OUTPUT);
   pinMode(TENSAO, INPUT);
   pinMode(CORRENTE, INPUT);
+
   Serial.begin(9600);
 }
 
 float potenciaAnterior = 0;
 int razaoCiclica = 100;
 int razaoCiclicaAnterior = 100;
+int step = 10;
 
 void loop() {
   analogWrite(PINO, razaoCiclica);
   delay(2000);
-  float tensao = (analogRead(TENSAO)*0.0048828125) * 1000 / 110;
+  float tensao = (analogRead(TENSAO) * 0.0048828125) * 1000 / 110;
   float corrente = (analogRead(CORRENTE) - 512) * (0.05859375);
-  float potenciaAtual = tensao * corrente;
+  float potencia = tensao * corrente;
 
-  Serial.print(tensao);
-  Serial.print(" - ");
-  Serial.print(corrente);
-  Serial.print(" - ");
-  Serial.print(potenciaAtual);
-  Serial.print(" - ");
-  Serial.print(razaoCiclica);
-  Serial.println();
+  Serial << "V: " << tensao << " I: " << corrente << " P: " << potencia << " D: " << razaoCiclica; 
+  
+  bool potenciaAumentou = potencia >= potenciaAnterior;
+  bool razaoCiclicaAumentou = razaoCiclica >= razaoCiclicaAnterior;  
 
-  if ((potenciaAtual >= potenciaAnterior) && (razaoCiclica >= razaoCiclicaAnterior)) {
-    razaoCiclicaAnterior = razaoCiclica;
-    razaoCiclica += 10;
-  } else if ((potenciaAtual < potenciaAnterior) && (razaoCiclica >= razaoCiclicaAnterior)) {
-    razaoCiclicaAnterior = razaoCiclica;
-    razaoCiclica -= 10;
-  } else if ((potenciaAtual >= potenciaAnterior) && (razaoCiclica <= razaoCiclicaAnterior)) {
-    razaoCiclicaAnterior = razaoCiclica;
-    razaoCiclica -= 10;
-  } else if ((potenciaAtual < potenciaAnterior) && (razaoCiclica <= razaoCiclicaAnterior)) {
-    razaoCiclicaAnterior = razaoCiclica;
-    razaoCiclica += 10;
-  }
+  float sinalStep = (potenciaAnterior == razaoCiclicaAumentou) ? 1.0 : -1.0;
+
+  razaoCiclicaAnterior = razaoCiclica;
+  razaoCiclica += step * sinalStep;
   
-  
-  potenciaAnterior = potenciaAtual;
-  if (razaoCiclica > 255) {
-    razaoCiclica = 255;
-  } else if (razaoCiclica < 0) {
-    razaoCiclica = 0;
-  }
+  potenciaAnterior = potencia;
+  razaoCiclica = constrain(razaoCiclica, 0, 255);
 }
